a5bffb4fcb0c42b7503800479052abd71129fdb6
Kevin Kobata <akirobot@users.noreply.github.com>
2022-05-13 16:42:45 -0700

    Beginnings of stat modification
    Tiles and Buffs now displayed
    Skill1 now displayed

--- js/shotgun2StarCheck.js
+++ js/shotgun2StarCheck.js
@@ -1,77 +1,77 @@
 function SG2Star(dollCase){
     switch (dollCase){
         case 1:
-            if (dollSearch1.value != 'SG'){
+            if (typeSelect1.value != 'SG'){
                 document.getElementById("2star1").removeAttribute("hidden");
             }
-            if (dollSearch1.value == 'SG'){
+            if (typeSelect1.value == 'SG'){
                 document.getElementById("2star1").setAttribute("hidden", "")
             }
-            if (raritySearch1.value != 2){
-                document.getElementById("shotgun1").removeAttribute("hidden");
+            if (raritySelect1.value != 2){
+                document.getElementById("SG1").removeAttribute("hidden");
             }
-            if (raritySearch1.value == 2){
-                document.getElementById("shotgun1").setAttribute("hidden", "");
+            if (raritySelect1.value == 2){
+                document.getElementById("SG1").setAttribute("hidden", "");
             }
         break;
         
         case 2:
-            if (dollSearch2.value != 'SG'){
+            if (typeSelect2.value != 'SG'){
                 document.getElementById("2star2").removeAttribute("hidden");
             }
-            if (dollSearch2.value == 'SG'){
+            if (typeSelect2.value == 'SG'){
                 document.getElementById("2star2").setAttribute("hidden", "")
             }
-            if (raritySearch2.value != 2){
-                document.getElementById("shotgun2").removeAttribute("hidden");
+            if (raritySelect2.value != 2){
+                document.getElementById("SG2").removeAttribute("hidden");
             }
-            if (raritySearch2.value == 2){
-                document.getElementById("shotgun2").setAttribute("hidden", "");
+            if (raritySelect2.value == 2){
+                document.getElementById("SG2").setAttribute("hidden", "");
             }
         break;
         
         case 3:
-            if (dollSearch3.value != 'SG'){
+            if (typeSelect3.value != 'SG'){
                 document.getElementById("2star3").removeAttribute("hidden");
             }
-            if (dollSearch3.value == 'SG'){
+            if (typeSelect3.value == 'SG'){
                 document.getElementById("2star3").setAttribute("hidden", "")
             }
-            if (raritySearch3.value != 2){
-                document.getElementById("shotgun3").removeAttribute("hidden");
+            if (raritySelect3.value != 2){
+                document.getElementById("SG3").removeAttribute("hidden");
             }
-            if (raritySearch3.value == 2){
-                document.getElementById("shotgun3").setAttribute("hidden", "");
+            if (raritySelect3.value == 2){
+                document.getElementById("SG3").setAttribute("hidden", "");
             }
         break;
         
         case 4:
-            if (dollSearch4.value != 'SG'){
+            if (typeSelect4.value != 'SG'){
                 document.getElementById("2star4").removeAttribute("hidden");
             }
-            if (dollSearch4.value == 'SG'){
+            if (typeSelect4.value == 'SG'){
                 document.getElementById("2star4").setAttribute("hidden", "")
             }
-            if (raritySearch4.value != 2){
-                document.getElementById("shotgun4").removeAttribute("hidden");
+            if (raritySelect4.value != 2){
+                document.getElementById("SG4").removeAttribute("hidden");
             }
-            if (raritySearch4.value == 2){
-                document.getElementById("shotgun4").setAttribute("hidden", "");
+            if (raritySelect4.value == 2){
+                document.getElementById("SG4").setAttribute("hidden", "");
             }
         break;
         
         case 5:
-            if (dollSearch5.value != 'SG'){
+            if (typeSelect5.value != 'SG'){
                 document.getElementById("2star5").removeAttribute("hidden");
             }
-            if (dollSearch5.value == 'SG'){
+            if (typeSelect5.value == 'SG'){
                 document.getElementById("2star5").setAttribute("hidden", "")
             }
-            if (raritySearch5.value != 2){
-                document.getElementById("shotgun5").removeAttribute("hidden");
+            if (raritySelect5.value != 2){
+                document.getElementById("SG5").removeAttribute("hidden");
             }
-            if (raritySearch5.value == 2){
-                document.getElementById("shotgun5").setAttribute("hidden", "");
+            if (raritySelect5.value == 2){
+                document.getElementById("SG5").setAttribute("hidden", "");
             }
         break;
     }
